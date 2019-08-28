71671ce1e2a5b5bd20cc39dba2166ae8ec90b5de
luis delgado <lad34@njit.edu>
2019-08-28 15:50:11 -0400

    bootstrap added

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import { Button } from 'reactstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Luis Delgado</h1>
+      <Button color="danger">Danger!</Button>
     </div>
   );
 }
