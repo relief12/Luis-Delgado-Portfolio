badaf26c0fca99326406bea5b7171d3e2cfa2810
luis delgado <lad34@njit.edu>
2019-08-28 17:09:09 -0400

    routing added

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -2,10 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from './serviceWorker';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import LandingPage from './Pages/LandingPage';
+import AboutMe from './Pages/AboutMePage';
+import Contact from './Pages/ContactPage';
+import Projects from './Pages/Projects';
+import Resume from './Pages/Resume';
+
+const routing = (
+    <Router>
+        <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/aboutme" component={AboutMe} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/resume" component={Resume} />
+        </Switch>
+    </Router>
+)
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
