1f03cc44a0c2b11bb7649b44f1aafdd192c12974
luis delgado <lad34@njit.edu>
2019-08-29 09:27:26 -0400

    navbar added with css for gradient

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,14 +1,71 @@
 import React from 'react';
-import { Button } from 'reactstrap';
 import './App.css';
+import MainPage from './Pages/MainPage';
+import { Link } from 'react-router-dom';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Luis Delgado</h1>
-      <Button color="danger">Danger!</Button>
-    </div>
-  );
-}
 
-export default App;
+   class App extends React.Component {
+    constructor(props) {
+      super(props);
+  
+      this.toggle = this.toggle.bind(this);
+      this.state = {
+        isOpen: false
+      };
+    }
+    toggle() {
+      this.setState({
+        isOpen: !this.state.isOpen
+      });
+    }
+    render() {
+      return (
+        <div>
+          <Navbar className="navbar-color" dark expand="md">
+            <NavbarBrand href="/">reactstrap</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="/components/">Components</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                </NavItem>
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle nav caret>
+                    Options
+                  </DropdownToggle>
+                  <DropdownMenu right>
+                    <DropdownItem>
+                      Option 1
+                    </DropdownItem>
+                    <DropdownItem>
+                      Option 2
+                    </DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem>
+                      Reset
+                    </DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </div>
+      );
+    }
+  }
+  export default App;
--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -12,4 +12,4 @@ ReactDOM.render(
   </BrowserRouter>
   , document.getElementById('root'));
 
-serviceWorker.unregister();
+serviceWorker.register();
