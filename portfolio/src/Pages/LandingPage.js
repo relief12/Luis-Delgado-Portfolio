73cddd0ce8f783ed5ca2b0c72a67b14eab51b929
luis delgado <lad34@njit.edu>
2019-08-29 23:30:26 -0400

    avatar added

--- portfolio/src/Pages/LandingPage.js
+++ portfolio/src/Pages/LandingPage.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 import { Container, Row, Col } from "reactstrap";
+import '../App.css'
 class LandingPage extends Component {
   render() {
     return (
-      <div style={{ width: '100%', margin: 'auto'}}>
-        <Container className="landing-container">
-          <Row>
-            <Col>.col</Col>
-           
+      <div  className="landing-container" style={{ width: '100%', marginTop:'32px'}}>
+       
+        <Container fluid>
+          <Row  >
+            <Col md={12}>
+              <img className="avatar-img" 
+              src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortFlat&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Happy&eyebrowType=UpDown&mouthType=Smile&skinColor=Light"/>
+             
+            </Col>
+            
+         
           </Row>
         </Container>
       </div>
