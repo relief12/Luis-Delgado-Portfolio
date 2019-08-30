4cfd94c79f7e1253e8d8b7a825f82fbca909ac3c
Relze <lad34@njit.edu>
2019-08-29 21:42:08 -0400

    adding landpage grid

--- portfolio/src/Pages/LandingPage.js
+++ portfolio/src/Pages/LandingPage.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
-
-class LandingPage extends  Component{
-    render(){
-        return(
-            <div style={{marginTop:'64px'}}><h1>Landing Page</h1></div>
-        )
-            
-        
-    }
-
+import React, { Component } from "react";
+import { Container, Row, Col } from "reactstrap";
+class LandingPage extends Component {
+  render() {
+    return (
+      <div style={{ width: '100%', margin: 'auto'}}>
+        <Container className="landing-container">
+          <Row>
+            <Col>.col</Col>
+           
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 export default LandingPage;
