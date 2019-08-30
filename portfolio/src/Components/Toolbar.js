aac1980b70a7edb6ce00fa09db0ec663444f6b98
luis delgado <lad34@njit.edu>
2019-08-30 14:32:56 -0400

    last push before i leave

--- portfolio/src/Components/Toolbar.js
+++ portfolio/src/Components/Toolbar.js
@@ -15,17 +15,20 @@ const Toolbar = props => (
       <div className="spacer" />
       <div className="toolbar_navigation-items">
         <ul>
-          <li>
-            <a href="/resume">Resume</a>
+        <li>
+            <a href="/">Home</a>
           </li>
           <li>
-            <a href="/aboutme">About me</a>
+            <a href="luis-delgado-portfolio/resume">Resume</a>
           </li>
           <li>
-            <a href="/projects">Projects</a>
+            <a href="luis-delgado-portfolio/aboutme">About me</a>
           </li>
           <li>
-            <a href="/contact">Contact Me</a>
+            <a href="luis-delgado-portfolio/projects">Projects</a>
+          </li>
+          <li>
+            <a href="luis-delgado-portfolio/contact">Contact Me</a>
           </li>
         </ul>
       </div>
