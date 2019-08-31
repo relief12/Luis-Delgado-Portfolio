154d6cfd35ea19c6baaa4570524c4132261a0279
Relze <lad34@njit.edu>
2019-08-31 11:22:24 -0400

    Updates

--- precache-manifest.6ef322663e7439fd4d2c0bac3bcde04c.js
+++ precache-manifest.6ef322663e7439fd4d2c0bac3bcde04c.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "56a94041eaf2b18b36665363f695bc3e",
+    "revision": "ecb712f593723d37bc61bc88254c442f",
     "url": "/luis-delgado-portfolio/index.html"
   },
   {
-    "revision": "0def8058470fd441941f",
+    "revision": "448a1af1eded82ca907a",
     "url": "/luis-delgado-portfolio/static/css/2.266e55a5.chunk.css"
   },
   {
-    "revision": "88b270b184d1d21f2436",
-    "url": "/luis-delgado-portfolio/static/css/main.a19280e9.chunk.css"
+    "revision": "375d451739c24d1d642c",
+    "url": "/luis-delgado-portfolio/static/css/main.ee47fae8.chunk.css"
   },
   {
-    "revision": "0def8058470fd441941f",
-    "url": "/luis-delgado-portfolio/static/js/2.46e4974e.chunk.js"
+    "revision": "448a1af1eded82ca907a",
+    "url": "/luis-delgado-portfolio/static/js/2.80e1af7a.chunk.js"
   },
   {
-    "revision": "88b270b184d1d21f2436",
-    "url": "/luis-delgado-portfolio/static/js/main.babc0b8c.chunk.js"
+    "revision": "375d451739c24d1d642c",
+    "url": "/luis-delgado-portfolio/static/js/main.b131ee58.chunk.js"
   },
   {
     "revision": "cd909e3e1e07feaf160b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/luis-delgado-portfolio/precache-manifest.ecc43837a0e9515b57dce25c9a1ec4cc.js"
+  "/luis-delgado-portfolio/precache-manifest.6ef322663e7439fd4d2c0bac3bcde04c.js"
 );
 
 self.addEventListener('message', (event) => {
